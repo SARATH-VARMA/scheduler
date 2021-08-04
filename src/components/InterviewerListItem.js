78d9b1fe16e203c83a759a01746d45b42528edd6
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-04 14:41:27 -0400

    Refactoring Interviewer Components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,8 +8,8 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
-      <img
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+    <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
