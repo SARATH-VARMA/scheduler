ffac0adbbf1e6e09489e978608ef0d2ba0359ce2
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-04 19:11:48 -0400

    Building and adding state to the Appointment Form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -122,14 +123,14 @@ storiesOf("Button", module)
         .add("Initial", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            onChange={action("onChange")}
           />
         ))
         .add("Preselected", () => (
           <InterviewerList
             interviewers={interviewers}
-            interviewer={3}
-            setInterviewer={action("setInterviewer")}
+            value={3}
+            onChange={action("onChange")}
           />
         )); 
         storiesOf("Appointment", module)
@@ -156,4 +157,20 @@ storiesOf("Button", module)
     />
   ))
   .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />); 
+  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
+  .add("Edit", () => (
+    <Form 
+      name="John" 
+      interviewers={interviewers}
+      interviewer={3} 
+      onSave={action("onSave")}
+      onCancel={action("onCancel")} 
+    />
+  ))
+  .add("Create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")} 
+    />
+  )); 
