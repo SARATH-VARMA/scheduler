bf3ef34fb72546f431f4eaf11c84d9913d80b1f2
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-09 19:04:00 -0400

    Building a Hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    if (replace) {
+      const newHistory = history.slice(0, history.length - 1);
+      setHistory([...newHistory, newMode]);
+    } else {
+      setHistory([...history, newMode]);
+    }
+  }
+
+  function back() { 
+    if (history.length > 1) {
+      const newHistory = history.slice(0, (history.length - 1));
+      setHistory([...newHistory]);
+      setMode(newHistory[newHistory.length - 1]);
+    }   
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}; 
